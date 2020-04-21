d0eff6fb1f779c4667009704d3ed87c4311b2326
Milon9787 <56488424+Milon9787@users.noreply.github.com>
2020-04-22 05:28:14 +0600

    responsive done

---
+++ privacy-policy.php
@@ -0,0 +1,276 @@
+<?php 
+include('function.php');
+$current_location  = get_current_location();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0">
+	<title>To My Daughter Heart Necklace &ndash; Shopping Namara</title>
+  <link rel='icon' type='image/png' href='favicon.ico'></link>
+  <meta name="robots" content="noindex,nofollow">
+  <meta name="robots" content="noindex">
+  <meta name="robots" content="nofollow">
+  <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="assets/fonts/font-awesome/fontawesome.min.css">
+  <link rel="stylesheet" type="text/css" href="assets/icofont/icofont.min.css">
+  <link rel="stylesheet" type="text/css" href="assets/slick.slider/slick-theme.css">
+  <link rel="stylesheet" type="text/css" href="assets/slick.slider/slick.css">  
+  <link href="https://fonts.googleapis.com/css2?family=Arapey&family=Lato&family=Montserrat:wght@300;400;700&family=Old+Standard+TT:ital,wght@0,400;0,700;1,400&family=Poppins:wght@300;400;600&display=swap" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<header class="header">
+  <div class="hdr-top">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-12">
+          <div class="hdr-top-inr">
+           <span class="flag_img">
+            <img src="https://www.countryflags.io/<?php echo $current_location['countryCode'] ?>/flat/24.png" class="lazy" data-src="https://www.countryflags.io/<?php echo $current_location['countryCode'] ?>/flat/24.png">
+          </span>   
+          <span class="flag_city"><?php echo $current_location['city'] ?></span>  
+          <span class="header-headline">- 70% OFF EXPIRES TODAY</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="hdr-top" style="display: none;">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-12">
+          <div class="hdr-top-inr">
+            <span>Free Shipping For Orders Over <b>$75</b></span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="hdr-btm">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-12">
+          <div class="hdr-btm-inr clearfix">
+            <div class="xs-menu-btn-bar show-sm clearfix">
+                <div class="nav-opener">
+                 <div class="nav-opener-btn">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                  </div>
+               </div>
+            </div>
+            <div class="hdr-btm-lft">
+              <div class="logo">
+                <a href="#">
+                  <img width="250" height="92" class="lazy" data-src="assets/images/logo.png">
+                </a>
+              </div>
+            </div>
+            <div class="hdr-btm-rgt clearfix">
+              <div class="hdr-cart-icon">
+                <span><!-- <img src="assets/images/cart-icon.png"> --><i class="icofont-shopping-cart"></i></span>
+              </div>
+              <div class="search-form-btn hide-sm"><i class="icofont-search"></i></div>
+              <nav class="main-nav hide-sm">
+                <ul class="clearfix">
+                  <li class="menu-item-has-children">
+                    <a href="#">Shop<i class="fas fa-sort-down"></i></a>
+                    <ul class="sub-menu">
+                      <li><a href="#">Featured Products</a></li>
+                      <li><a href="#">Featured Products</a></li>
+                      <li><a href="#">Featured Products</a></li>
+                      <li><a href="#">Lorem Ipsum</a></li>
+                    </ul>
+                  </li>
+                  <li><a href="#">Contact </a></li>
+                </ul>
+              </nav>
+              <div class="search-bar"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</header><!-- end of header -->
+
+<div class="search-form-wrp-clr">
+  <div class="search-form-wrp">
+    <span class="close-btn"><i class="fas fa-times"></i></span>
+    <div class="search-form">
+      <input class="search" type="text" name="search" placeholder="Search our store">
+      <input class="search-btn" type="submit" value="">
+      <i class="fas fa-search"></i>
+    </div>
+  </div>
+</div>
+
+
+<!-- - xs menu bar and pop up menu -- -->
+ 
+  <div class="xs-popup-main-menu-wrap">
+    <div class="xs-popup-main-menu-ftr clearfix">
+      <div class="xs-menu-close-btn-controller clearfix">
+         <div class="fl-close-btn">
+           <span></span>
+           <span></span>
+           <span></span>
+         </div>
+      </div>
+    </div>
+    <nav class="xs-popup-main-nav clearfix">
+      <ul class="clearfix">
+        <li class="menu-item-has-children">
+          <a href="#">Shop</a>
+          <ul class="clearfix sub-menu">
+            <li><a href="#">Featured Products</a> </li>
+          </ul>
+        </li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+    </nav>
+    <div class="xs-popup-btm-menu">
+      <ul>
+        <li><a href="#">FAQ</a></li>
+        <li><a href="#">Shipping & Returns</a></li>
+        <li><a href="#">Privacy Policy</a></li>
+        <li><a href="#">Terms of Service</a></li>
+        <li><a href="#">Contact Us</a></li>
+      </ul>
+    </div>
+  </div>
+
+<!-- - end of xs menu bar and pop up menu -- -->
+
+
+
+
+
+<section class="sn-page-cntlr">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="sn-block-650">
+          <div class="sn-page-sec-entry-hdr">
+            <h1>Privacy Policy</h1>
+          </div>
+          <div class="sn-sec-subtitle">
+            <strong>PRIVACY STATEMENT</strong>
+            <span>-----</span>
+          </div>
+          <div class="sn-text-module-left-title">
+            <h5>SECTION 1 - WHAT DO WE DO WITH YOUR INFORMATION?</h5>
+            <p>When you purchase something from our store, as part of the buying and selling process, we collect the personal information you give us such as your name, address and email address. <br>
+            When you browse our store, we also automatically receive your computer’s internet protocol (IP) address in order to provide us with information that helps us learn about your browser and operating system. <br> Email marketing (if applicable): With your permission, we may send you emails about our store, new products and other updates.
+            </p>
+          </div>
+          <div class="sn-text-module-left-title">
+            <h5>SECTION 2 - CONSENT</h5>
+            <p>How do you get my consent? <br>When you provide us with personal information to complete a transaction, verify your credit card, place an order, arrange for a delivery or return a purchase, we imply that you consent to our collecting it and using it for that specific reason only.<br>
+            If we ask for your personal information for a secondary reason, like marketing, we will either ask you directly for your expressed consent, or provide you with an opportunity to say no.</p>
+            <p>How do I withdraw my consent? <br>
+              If after you opt-in, you change your mind, you may withdraw your consent for us to contact you, for the continued collection, use or disclosure of your information, at anytime, by contacting us at support@shoppingnamara.com
+            </p>
+          </div>
+
+          <div class="sn-text-module-left-title">
+            <h5>SECTION 3 - DISCLOSURE</h5>
+            <p>We may disclose your personal information if we are required by law to do so or if you violate our Terms of Service.</p>
+          </div>
+          <div class="sn-text-module-left-title">
+            <h5>SECTION 4 - SHOPIFY</h5>
+            <p>Our store is hosted on Shopify Inc. They provide us with the online e-commerce platform that allows us to sell our products and services to you.<br>
+            Your data is stored through Shopify’s data storage, databases and the general Shopify application. They store your data on a secure server behind a firewall.</p>
+            <p>Payment: <br>
+              If you choose a direct payment gateway to complete your purchase, then Shopify stores your credit card data. It is encrypted through the Payment Card Industry Data Security Standard (PCI-DSS). Your purchase transaction data is stored only as long as is necessary to complete your purchase transaction. After that is complete, your purchase transaction information is deleted. <br>All direct payment gateways adhere to the standards set by PCI-DSS as managed by the PCI Security Standards Council, which is a joint effort of brands like Visa, MasterCard, American Express and Discover. <br>PCI-DSS requirements help ensure the secure handling of credit card information by our store and its service providers. <br> For more insight, you may also want to read Shopify’s Terms of Service (https://www.shopify.com/legal/terms) or Privacy Statement (https://www.shopify.com/legal/privacy).
+            </p>
+          </div>
+          <div class="sn-text-module-left-title">
+            <h5>SECTION 5 - THIRD-PARTY SERVICES</h5>
+            <p>In general, the third-party providers used by us will only collect, use and disclose your information to the extent necessary to allow them to perform the services they provide to us. <br>
+              However, certain third-party service providers, such as payment gateways and other payment transaction processors, have their own privacy policies in respect to the information we are required to provide to them for your purchase-related transactions. <br>
+              For these providers, we recommend that you read their privacy policies so you can understand the manner in which your personal information will be handled by these providers. <br> In particular, remember that certain providers may be located in or have facilities that are located a different jurisdiction than either you or us. So if you elect to proceed with a transaction that involves the services of a third-party service provider, then your information may become subject to the laws of the jurisdiction(s) in which that service provider or its facilities are located. <br> As an example, if you are located in Canada and your transaction is processed by a payment gateway located in the United States, then your personal information used in completing that transaction may be subject to disclosure under United States legislation, including the Patriot Act.<br> Once you leave our store’s website or are redirected to a third-party website or application, you are no longer governed by this Privacy Policy or our website’s Terms of Service.</p>
+              <p>Links<br>
+              When you click on links on our store, they may direct you away from our site. We are not responsible for the privacy practices of other sites and encourage you to read their privacy statements.</p>
+          </div>
+          <div class="sn-text-module-left-title">
+            <h5>SECTION 6 - SECURITY</h5>
+            <p>To protect your personal information, we take reasonable precautions and follow industry best practices to make sure it is not inappropriately lost, misused, accessed, disclosed, altered or destroyed.<br>
+            If you provide us with your credit card information, the information is encrypted using secure socket layer technology (SSL) and stored with a AES-256 encryption. Although no method of transmission over the Internet or electronic storage is 100% secure, we follow all PCI-DSS requirements and implement additional generally accepted industry standards.</p>
+          </div>
+          <div class="sn-text-module-left-title">
+            <h5>SECTION 7 - COOKIES</h5>
+            <p>Here is a list of cookies that we use. We’ve listed them here so you that you can choose if you want to opt-out of cookies or not.<br>_session_id, unique token, sessional, Allows Shopify to store information about your session (referrer, landing page, etc). <br>shopify_visit, no data held, Persistent for 30 minutes from the last visit, Used by our website provider’s internal stats tracker to record the number of visits <br>_shopify_uniq, no data held, expires midnight (relative to the visitor) of the next day, Counts the number of visits to a store by a single customer.<br>cart, unique token, persistent for 2 weeks, Stores information about the contents of your cart. _secure_session_id, unique token, sessional<br>storefront_digest, unique token, indefinite If the shop has a password, this is used to determine if the current visitor has access.</p>
+          </div>
+          <div class="sn-text-module-left-title">
+            <h5>SECTION 8 - AGE OF CONSENT</h5>
+            <p>By using this site, you represent that you are at least the age of majority in your state or province of residence, or that you are the age of majority in your state or province of residence and you have given us your consent to allow any of your minor dependents to use this site.</p>
+          </div>
+          <div class="sn-text-module-left-title">
+            <h5>SECTION 9 - CHANGES TO THIS PRIVACY POLICY</h5>
+            <p>We reserve the right to modify this privacy policy at any time, so please review it frequently. Changes and clarifications will take effect immediately upon their posting on the website. If we make material changes to this policy, we will notify you here that it has been updated, so that you are aware of what information we collect, how we use it, and under what circumstances, if any, we use and/or disclose it.<br>If our store is acquired or merged with another company, your information may be transferred to the new owners so that we may continue to sell products to you.</p>
+          </div>
+          <div class="sn-text-module-left-title">
+            <h5>QUESTIONS AND CONTACT INFORMATION</h5>
+            <p>If you would like to: access, correct, amend or delete any personal information we have about you, register a complaint, or simply want more information contact us at support@shoppingnamara.com</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+<footer class="footer-wrp">
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-12">
+        <div class="ftr-col-inr clearfix">
+          <div class="ftr-col-lft">
+            <div class="ftr-col-menu">
+              <ul>
+                <li><a href="#">FAQ</a></li>
+                <li><a href="#">Shipping & Returns</a></li>
+                <li><a href="#">Privacy Policy</a></li>
+                <li><a href="#">Terms of Service</a></li>
+                <li><a href="#">Contact Us</a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="ftr-col-rgt clearfix">
+            <div class="ftr-col-rgt-dsc">
+              <p> &copy;2020, Shopping Namara</p>
+              <span>Powered by Shopify</span>
+            </div>
+            <div class="ftr-col-rgt-cart clearfix">
+              <ul class="clearfix">
+                <li><i class="icofont-american-express-alt"></i></li>
+                <li><i class="icofont-diners-club"></i></li>
+                <li><i class="icofont-discover"></i></li>
+                <li><i class="icofont-jcb-alt"></i></li>
+                <li><i class="icofont-mastercard"></i></li>
+                <li><i class="icofont-paypal"></i></li>
+                <li><i class="icofont-visa"></i></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div><!-- end of ftr-top --> 
+</footer><!-- end of footer -->
+
+<script src="https://code.jquery.com/jquery-3.0.0.js"></script>
+<script src="https://code.jquery.com/jquery-migrate-3.0.0.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+<script src="assets/slick.slider/slick.js"></script>
+<script src="assets/js/packery.pkgd.min.js"></script>
+<script src="assets/js/lazyload.min.js"></script>
+<script src="assets/js/main.js"></script>
+<script type="text/javascript">window.onload = function() {var newd1 = new LazyLoad({elements_selector: ".lazy"});}</script>
+</body>
+</html>
---
+++ shipping-returns.php
@@ -0,0 +1,252 @@
+<?php 
+include('function.php');
+$current_location  = get_current_location();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0">
+	<title>To My Daughter Heart Necklace &ndash; Shopping Namara</title>
+  <link rel='icon' type='image/png' href='favicon.ico'></link>
+  <meta name="robots" content="noindex,nofollow">
+  <meta name="robots" content="noindex">
+  <meta name="robots" content="nofollow">
+  <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="assets/fonts/font-awesome/fontawesome.min.css">
+  <link rel="stylesheet" type="text/css" href="assets/icofont/icofont.min.css">
+  <link rel="stylesheet" type="text/css" href="assets/slick.slider/slick-theme.css">
+  <link rel="stylesheet" type="text/css" href="assets/slick.slider/slick.css">  
+  <link href="https://fonts.googleapis.com/css2?family=Arapey&family=Lato&family=Montserrat:wght@300;400;700&family=Old+Standard+TT:ital,wght@0,400;0,700;1,400&family=Poppins:wght@300;400;600&display=swap" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<header class="header">
+  <div class="hdr-top">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-12">
+          <div class="hdr-top-inr">
+           <span class="flag_img">
+            <img src="https://www.countryflags.io/<?php echo $current_location['countryCode'] ?>/flat/24.png" class="lazy" data-src="https://www.countryflags.io/<?php echo $current_location['countryCode'] ?>/flat/24.png">
+          </span>   
+          <span class="flag_city"><?php echo $current_location['city'] ?></span>  
+          <span class="header-headline">- 70% OFF EXPIRES TODAY</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="hdr-top" style="display: none;">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-12">
+          <div class="hdr-top-inr">
+            <span>Free Shipping For Orders Over <b>$75</b></span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="hdr-btm">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-12">
+          <div class="hdr-btm-inr clearfix">
+            <div class="xs-menu-btn-bar show-sm clearfix">
+                <div class="nav-opener">
+                 <div class="nav-opener-btn">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                  </div>
+               </div>
+            </div>
+            <div class="hdr-btm-lft">
+              <div class="logo">
+                <a href="#">
+                  <img width="250" height="92" class="lazy" data-src="assets/images/logo.png">
+                </a>
+              </div>
+            </div>
+            <div class="hdr-btm-rgt clearfix">
+              <div class="hdr-cart-icon">
+                <span><!-- <img src="assets/images/cart-icon.png"> --><i class="icofont-shopping-cart"></i></span>
+              </div>
+              <div class="search-form-btn hide-sm"><i class="icofont-search"></i></div>
+              <nav class="main-nav hide-sm">
+                <ul class="clearfix">
+                  <li class="menu-item-has-children">
+                    <a href="#">Shop<i class="fas fa-sort-down"></i></a>
+                    <ul class="sub-menu">
+                      <li><a href="#">Featured Products</a></li>
+                      <li><a href="#">Featured Products</a></li>
+                      <li><a href="#">Featured Products</a></li>
+                      <li><a href="#">Lorem Ipsum</a></li>
+                    </ul>
+                  </li>
+                  <li><a href="#">Contact </a></li>
+                </ul>
+              </nav>
+              <div class="search-bar"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</header><!-- end of header -->
+
+<div class="search-form-wrp-clr">
+  <div class="search-form-wrp">
+    <span class="close-btn"><i class="fas fa-times"></i></span>
+    <div class="search-form">
+      <input class="search" type="text" name="search" placeholder="Search our store">
+      <input class="search-btn" type="submit" value="">
+      <i class="fas fa-search"></i>
+    </div>
+  </div>
+</div>
+
+
+<!-- - xs menu bar and pop up menu -- -->
+ 
+  <div class="xs-popup-main-menu-wrap">
+    <div class="xs-popup-main-menu-ftr clearfix">
+      <div class="xs-menu-close-btn-controller clearfix">
+         <div class="fl-close-btn">
+           <span></span>
+           <span></span>
+           <span></span>
+         </div>
+      </div>
+    </div>
+    <nav class="xs-popup-main-nav clearfix">
+      <ul class="clearfix">
+        <li class="menu-item-has-children">
+          <a href="#">Shop</a>
+          <ul class="clearfix sub-menu">
+            <li><a href="#">Featured Products</a> </li>
+          </ul>
+        </li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+    </nav>
+    <div class="xs-popup-btm-menu">
+      <ul>
+        <li><a href="#">FAQ</a></li>
+        <li><a href="#">Shipping & Returns</a></li>
+        <li><a href="#">Privacy Policy</a></li>
+        <li><a href="#">Terms of Service</a></li>
+        <li><a href="#">Contact Us</a></li>
+      </ul>
+    </div>
+  </div>
+
+<!-- - end of xs menu bar and pop up menu -- -->
+
+
+
+
+
+<section class="sn-page-cntlr">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="sn-block-650">
+          <div class="sn-page-sec-entry-hdr">
+            <h1>Shipping & Returns</h1>
+          </div>
+          <div class="sn-text-module-center-title sn-text-module-underline">
+            <h2>SHIPPING</h2>
+            <p>
+              <strong>What are my shipping options? </strong>
+              We offer standard shipping world-wide for $4.95. However, we offer FREE worldwide shipping for orders over $75! 
+            </p>
+            <p>We do offer FREE Worldwide Shipping on ALL ORDERS periodically so look out for this special deal! </p>
+            <p>
+              <strong>How long will it take to receive my order?</strong>
+            </p>
+            <p>Our shipping times are us follows.</p>
+             <ul>
+              <li>For the USA (including Hawaii and Alaska): you should receive your order within 2-4 weeks of placing it.</li>
+              <li>For everywhere else: you should receive your order within 3-5 weeks of placing it. The wide range is due to different policies of customs clearance in different countries. Most of our customers get their order within 4 weeks!</li>
+            </ul>
+            <p>We are a small, independent company so these are guidelines not hard and fast rules! Just contact us if you need an update estimated time of arrival for your order.</p>
+          </div>
+          <div class="sn-text-module-center-title sn-text-module-underline">
+            <h2>RETURNS</h2>
+            <p>
+              <strong style="display: inline-block;">FOR MOST PRODUCTS:</strong>
+              we offer a 30 day return policy. If you would like to return your product just email us and we will provide you with a return address. Please note, you are responsible for return costs.
+            </p>
+            <p>Please note that due to hygiene reasons, shapewear, underwear, and lingerie is not eligible for refunds unless it is due to a product defect.</p>
+
+            <p>We STRONGLY recommend looking at the sizing guidelines outlines in the product description before purchasing.  If you have questions regarding recommendations, you can simply email us at support@shoppingnamara.com.</p>
+
+            <p><strong>Exchanges</strong></p>
+            <p>
+              <strong>FOR MOST PRODUCTS:</strong>
+              we offer a 30 day exchange policy. If you would like to exchange your product just email us and we will provide you with a return address. Please note, you are responsible for return costs.
+            </p>
+            <p>Please note that due to hygiene reasons, shapewear, underwear, and lingerie is not eligible <br> for exchanges unless it is due to a product defect.</p>
+            <p>We STRONGLY recommend looking at the sizing guidelines outlines in the product description before purchasing.</p>
+          </div>
+          
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+<footer class="footer-wrp">
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-12">
+        <div class="ftr-col-inr clearfix">
+          <div class="ftr-col-lft">
+            <div class="ftr-col-menu">
+              <ul>
+                <li><a href="#">FAQ</a></li>
+                <li><a href="#">Shipping & Returns</a></li>
+                <li><a href="#">Privacy Policy</a></li>
+                <li><a href="#">Terms of Service</a></li>
+                <li><a href="#">Contact Us</a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="ftr-col-rgt clearfix">
+            <div class="ftr-col-rgt-dsc">
+              <p> &copy;2020, Shopping Namara</p>
+              <span>Powered by Shopify</span>
+            </div>
+            <div class="ftr-col-rgt-cart clearfix">
+              <ul class="clearfix">
+                <li><i class="icofont-american-express-alt"></i></li>
+                <li><i class="icofont-diners-club"></i></li>
+                <li><i class="icofont-discover"></i></li>
+                <li><i class="icofont-jcb-alt"></i></li>
+                <li><i class="icofont-mastercard"></i></li>
+                <li><i class="icofont-paypal"></i></li>
+                <li><i class="icofont-visa"></i></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div><!-- end of ftr-top --> 
+</footer><!-- end of footer -->
+
+<script src="https://code.jquery.com/jquery-3.0.0.js"></script>
+<script src="https://code.jquery.com/jquery-migrate-3.0.0.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+<script src="assets/slick.slider/slick.js"></script>
+<script src="assets/js/packery.pkgd.min.js"></script>
+<script src="assets/js/lazyload.min.js"></script>
+<script src="assets/js/main.js"></script>
+<script type="text/javascript">window.onload = function() {var newd1 = new LazyLoad({elements_selector: ".lazy"});}</script>
+</body>
+</html>
