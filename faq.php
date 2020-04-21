b3061e0df6eadfcf02fc74d20b286b021e0d7514
Rannojit <cserannojitsarkar22@gmail.com>
2020-04-22 01:00:55 +0600

    terms-of-service and faq page done

---
+++ faq.php
@@ -0,0 +1,261 @@
+<?php 
+include('function.php');
+$current_location  = get_current_location();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0">
+	<title>To My Daughter Heart Necklace &ndash; Shopping Namara</title>
+  <link rel='icon' type='image/png' href='favicon.ico'></link>
+  <meta name="robots" content="noindex,nofollow">
+  <meta name="robots" content="noindex">
+  <meta name="robots" content="nofollow">
+  <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="assets/fonts/font-awesome/fontawesome.min.css">
+  <link rel="stylesheet" type="text/css" href="assets/icofont/icofont.min.css">
+  <link rel="stylesheet" type="text/css" href="assets/slick.slider/slick-theme.css">
+  <link rel="stylesheet" type="text/css" href="assets/slick.slider/slick.css">  
+  <link href="https://fonts.googleapis.com/css2?family=Arapey&family=Lato&family=Montserrat:wght@300;400;700&family=Old+Standard+TT:ital,wght@0,400;0,700;1,400&family=Poppins:wght@300;400;600&display=swap" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<header class="header">
+  <div class="hdr-top">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-12">
+          <div class="hdr-top-inr">
+           <span class="flag_img">
+            <img src="https://www.countryflags.io/<?php echo $current_location['countryCode'] ?>/flat/24.png" class="lazy" data-src="https://www.countryflags.io/<?php echo $current_location['countryCode'] ?>/flat/24.png">
+          </span>   
+          <span class="flag_city"><?php echo $current_location['city'] ?></span>  
+          <span class="header-headline">- 70% OFF EXPIRES TODAY</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="hdr-top" style="display: none;">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-12">
+          <div class="hdr-top-inr">
+            <span>Free Shipping For Orders Over <b>$75</b></span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="hdr-btm">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-12">
+          <div class="hdr-btm-inr clearfix">
+            <div class="xs-menu-btn-bar show-sm clearfix">
+                <div class="nav-opener">
+                 <div class="nav-opener-btn">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                  </div>
+               </div>
+            </div>
+            <div class="hdr-btm-lft">
+              <div class="logo">
+                <a href="#">
+                  <img width="250" height="92" class="lazy" data-src="assets/images/logo.png">
+                </a>
+              </div>
+            </div>
+            <div class="hdr-btm-rgt clearfix">
+              <div class="hdr-cart-icon">
+                <span><!-- <img src="assets/images/cart-icon.png"> --><i class="icofont-shopping-cart"></i></span>
+              </div>
+              <div class="search-form-btn hide-sm"><i class="icofont-search"></i></div>
+              <nav class="main-nav hide-sm">
+                <ul class="clearfix">
+                  <li class="menu-item-has-children">
+                    <a href="#">Shop<i class="fas fa-sort-down"></i></a>
+                    <ul class="sub-menu">
+                      <li><a href="#">Featured Products</a></li>
+                      <li><a href="#">Featured Products</a></li>
+                      <li><a href="#">Featured Products</a></li>
+                      <li><a href="#">Lorem Ipsum</a></li>
+                    </ul>
+                  </li>
+                  <li><a href="#">Contact </a></li>
+                </ul>
+              </nav>
+              <div class="search-bar"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</header><!-- end of header -->
+
+<div class="search-form-wrp-clr">
+  <div class="search-form-wrp">
+    <span class="close-btn"><i class="fas fa-times"></i></span>
+    <div class="search-form">
+      <input class="search" type="text" name="search" placeholder="Search our store">
+      <input class="search-btn" type="submit" value="">
+      <i class="fas fa-search"></i>
+    </div>
+  </div>
+</div>
+
+
+<!-- - xs menu bar and pop up menu -- -->
+ 
+  <div class="xs-popup-main-menu-wrap">
+    <div class="xs-popup-main-menu-ftr clearfix">
+      <div class="xs-menu-close-btn-controller clearfix">
+         <div class="fl-close-btn">
+           <span></span>
+           <span></span>
+           <span></span>
+         </div>
+      </div>
+    </div>
+    <nav class="xs-popup-main-nav clearfix">
+      <ul class="clearfix">
+        <li class="menu-item-has-children">
+          <a href="#">Shop</a>
+          <ul class="clearfix sub-menu">
+            <li><a href="#">Featured Products</a> </li>
+          </ul>
+        </li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+    </nav>
+    <div class="xs-popup-btm-menu">
+      <ul>
+        <li><a href="#">FAQ</a></li>
+        <li><a href="#">Shipping & Returns</a></li>
+        <li><a href="#">Privacy Policy</a></li>
+        <li><a href="#">Terms of Service</a></li>
+        <li><a href="#">Contact Us</a></li>
+      </ul>
+    </div>
+  </div>
+
+<!-- - end of xs menu bar and pop up menu -- -->
+
+
+
+
+
+<section class="sn-page-cntlr">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="sn-block-650">
+          <div class="sn-page-sec-entry-hdr">
+            <h1>FAQ</h1>
+          </div>
+          <div class="sn-text-module-center-title">
+            <h2>PAYMENT</h2>
+            <p>
+              <strong>How do I pay for my order? Is it secure?</strong>
+              Our checkout process uses Shopify Payments, Authorize.net or PayPal, three of the most trusted online payment solutions on the web. 
+            </p>
+            <p>We accept Visa, Mastercard, Discover, American Express and Diners Club which adds another layer of protection. So all in, your order and your information is super secure!</p>
+          </div>
+          <div class="sn-text-module-center-title">
+            <h2>SHIPPING</h2>
+            <p><strong>What are my shipping options?</strong></p>
+            <p>We offer standard shipping world-wide for $4.95. However, we offer FREE worldwide shipping for orders over $75!</p>
+            <p>We do offer FREE Worldwide Shipping on ALL ORDERS periodically so look out for this special deal! </p>
+            <p>We will replace/refund any package which has any issues related to delivery/shipping (unless it is out of our control)!</p>
+            <p><strong>How long will it take to receive my order?</strong></p>
+            <p>Our shipping times are us follows.</p>
+            <ul>
+              <li>For the USA (including Hawaii and Alaska): you should receive your order within 2-4 weeks of placing it.</li>
+              <li>For everywhere else: you should receive your order within 3-6 weeks of placing it. The wide range is due to different policies of customs clearance in different countries. Most of our customers get their order within 4 weeks!</li>
+            </ul>
+            <p>We are a small, independent company so these are guidelines not hard and fast rules! Just contact us if you need an update estimated time of arrival for your order.</p>
+          </div>
+
+          <div class="sn-text-module-center-title">
+            <h2>AFTER ORDERING</h2>
+            <p><strong>I placed an order with Shopping Namara. Can I see its status? </strong></p>
+            <p>First off, thank you for ordering with us! </p>
+            <p>Once you place you order and it has been processed (generally 3-5 business days), you will receive an email with the order shipment tracking link.</p>
+            <p>If you still need assistance in locating your order, just email us at support@shoppingnamara.com. We will respond very quickly!</p>
+            <div style="height: 10px;"></div>
+            <p><strong>What is this store's exchange policy?</strong></p>
+            <p><strong style="display: inline-block;">FOR MOST PRODUCTS:</strong> we offer a 30 day exchange policy. If you would like to exchange your product just email us and we will provide you with a return address. <u>Please note, you are responsible for return costs.</u></p>
+            <p>Please note, any product which has intimate/health usage (shape-wear, exfoliators) are not covered due to hygiene concerns.</p>
+            <p>We STRONGLY recommend looking at the sizing guidelines outlines in the product description before purchasing.  If you have questions regarding recommendations, you can simply email us at support@shoppingnamara.com.</p>
+
+            <p><strong>What is this store's return policy?</strong></p>
+            <p><strong style="display: inline-block;">FOR MOST PRODUCTS:</strong> we offer a 30 day return policy. If you would like to return your product just email us and we will provide you with a return address. Please note, you are responsible for return costs.</p>
+            <p>Please note, any product which has intimate/health usage (shape-wear, exfoliators) are not covered due to hygiene concerns.</p>
+            <p>We STRONGLY recommend looking at the sizing guidelines outlines in the product description before purchasing.  </p>
+            <P><strong>My order hasn’t arrived yet, what do I do?</strong></P>
+            <p>If it is beyond the estimated shipping times since you have placed your order, just email us with your order number at support@shoppingnamara.com and we will start checking it ASAP.</p>
+            <p><strong>What if something is wrong with my order?</strong></p>
+            <p>If you have a problem with an order from this store, please get in touch with us directly at support@shoppingnamara.com ASAP and we will help you!</p>
+          </div>
+          
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+<footer class="footer-wrp">
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-12">
+        <div class="ftr-col-inr clearfix">
+          <div class="ftr-col-lft">
+            <div class="ftr-col-menu">
+              <ul>
+                <li><a href="#">FAQ</a></li>
+                <li><a href="#">Shipping & Returns</a></li>
+                <li><a href="#">Privacy Policy</a></li>
+                <li><a href="#">Terms of Service</a></li>
+                <li><a href="#">Contact Us</a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="ftr-col-rgt clearfix">
+            <div class="ftr-col-rgt-dsc">
+              <p> &copy;2020, Shopping Namara</p>
+              <span>Powered by Shopify</span>
+            </div>
+            <div class="ftr-col-rgt-cart clearfix">
+              <ul class="clearfix">
+                <li><i class="icofont-american-express-alt"></i></li>
+                <li><i class="icofont-diners-club"></i></li>
+                <li><i class="icofont-discover"></i></li>
+                <li><i class="icofont-jcb-alt"></i></li>
+                <li><i class="icofont-mastercard"></i></li>
+                <li><i class="icofont-paypal"></i></li>
+                <li><i class="icofont-visa"></i></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div><!-- end of ftr-top --> 
+</footer><!-- end of footer -->
+
+<script src="https://code.jquery.com/jquery-3.0.0.js"></script>
+<script src="https://code.jquery.com/jquery-migrate-3.0.0.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+<script src="assets/slick.slider/slick.js"></script>
+<script src="assets/js/packery.pkgd.min.js"></script>
+<script src="assets/js/lazyload.min.js"></script>
+<script src="assets/js/main.js"></script>
+<script type="text/javascript">window.onload = function() {var newd1 = new LazyLoad({elements_selector: ".lazy"});}</script>
+</body>
+</html>
